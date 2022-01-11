060db9df740363c3d10fda34e4ca6b9b41bd74f6
James Johnson <johnsojr@umn.edu>
2022-01-11 14:29:23 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello</h1>
+    <MySfcComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import MySfcComponent from "my-sfc-component";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    MySfcComponent,
+  },
+};
 </script>
 
 <style>
