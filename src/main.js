406cedf07b9902ffeb170295df42018c9b135e26
James Johnson <johnsojr@umn.edu>
2022-01-11 15:24:18 -0600

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+// import "./compositionApi.js";
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+Vue.use(VueCompositionAPI);
+// import Vue from "vue";
+import App from "./App.vue";
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
